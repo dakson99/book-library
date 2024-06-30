152b6933f835c1fd024da5918f4f9aa4365224d8
dakson99 <darko99.mikavica@gmail.com>
2024-06-30 22:55:10 +0200

    Move event listeners to constructor, bind the this keyword

--- script.js
+++ script.js
@@ -15,8 +15,6 @@ const btnForm = document.querySelector('.btn__form');
 const overlay = document.querySelector('.overlay');
 const closeIcon = document.querySelector('.close__icon');
 
-let mylibrary = [];
-
 // class
 class Book {
     constructor(Name, author, pages, read) {
@@ -32,109 +30,133 @@ class Book {
     }
 }
 
-// functions
-const addBookToLibrary = function (book) {
-    mylibrary.push(book);
-};
-const displayFormOverlay = function () {
-    formado.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-};
-const removeformOverlay = function () {
-    formado.classList.add('hidden');
-    overlay.classList.add('hidden');
-};
-const removeFormOverlayWindow = function () {
-    return function (e) {
-        if (e.key === 'Escape') {
-            removeformOverlay();
-        }
-    };
-};
-const submitForm = function () {
-    return function (e) {
-        e.preventDefault();
-        const title = formTitle.value;
-        const author = formAuthor.value;
-        const nPages = formNpages.value;
-        const entry = new Book(title, author, nPages, formCheck.checked);
-        addBookToLibrary(entry);
-        newCard();
+class Library {
+    #mylibrary;
 
-        formTitle.value = formAuthor.value = formNpages.value = '';
-        removeformOverlay();
-        textDiv.classList.add('text__div__transform');
-        listDiv.classList.add('list__div__transform');
-    };
-};
+    constructor() {
+        this.#mylibrary = [];
 
-const toggleReadStatus = function (el, btn) {
-    return function () {
-        el.changeRead();
-        btn.textContent = el.read === true ? 'Read ✔️' : 'Unread ❌';
-    };
-};
-const deleteBookCard = function (i) {
-    return function (e) {
-        const clicked = e.target.closest('.ul__list');
-        const toDelete = clicked.querySelector(`.list__item--${i + 1}`);
-        toDelete.remove();
-        mylibrary.pop();
-        if (mylibrary.length === 0)
-            textDiv.classList.remove('text__div__transform');
-    };
-};
+        // submit form
+        formado.addEventListener('submit', this.submitForm().bind(this));
 
-// display objects
-const newCard = function () {
-    while (list.firstChild) {
-        list.removeChild(list.firstChild);
+        // display form and modal
+        btnNewBook.addEventListener('click', this.displayFormOverlay.bind(this));
+
+        // remove form and modal
+        overlay.addEventListener('click', this.removeformOverlay.bind(this));
+        closeIcon.addEventListener('click', this.removeformOverlay.bind(this));
+
+        window.addEventListener('keydown', this.removeFormOverlayWindow.bind(this));
     }
 
-    mylibrary.forEach((el, i) => {
-        // li element
-        const newItem = document.createElement('li');
-        newItem.classList.add('list__item__style');
-        newItem.classList.add(`list__item--${i + 1}`);
-        newItem.innerHTML = `<p class="card__book__name">${el.Name}
+    addBookToLibrary = function (book) {
+        this.#mylibrary.push(book);
+    };
+
+    displayFormOverlay = function () {
+        formado.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    };
+
+    removeformOverlay = function () {
+        formado.classList.add('hidden');
+        overlay.classList.add('hidden');
+    };
+
+    removeFormOverlayWindow = function () {
+        return function (e) {
+            if (e.key === 'Escape') {
+                this.removeformOverlay();
+            }
+        };
+    };
+
+    submitForm = function () {
+        return function (e) {
+            e.preventDefault();
+            const title = formTitle.value;
+            const author = formAuthor.value;
+            const nPages = formNpages.value;
+            const entry = new Book(title, author, nPages, formCheck.checked);
+            this.addBookToLibrary(entry);
+            this.newCard();
+            formTitle.value = formAuthor.value = formNpages.value = '';
+            this.removeformOverlay();
+            textDiv.classList.add('text__div__transform');
+            listDiv.classList.add('list__div__transform');
+        };
+    };
+
+    toggleReadStatus = function (el, btn) {
+        return function () {
+            el.changeRead();
+            btn.textContent = el.read === true ? 'Read ✔️' : 'Unread ❌';
+        };
+    };
+
+    deleteBookCard = function (i) {
+        return function (e) {
+            const clicked = e.target.closest('.ul__list');
+            const toDelete = clicked.querySelector(`.list__item--${i + 1}`);
+            toDelete.remove();
+            this.#mylibrary.pop();
+            if (this.#mylibrary.length === 0)
+                textDiv.classList.remove('text__div__transform');
+        };
+    };
+
+    // display objects
+    newCard = function () {
+        while (list.firstChild) {
+            list.removeChild(list.firstChild);
+        }
+
+        this.#mylibrary.forEach((el, i) => {
+            // li element
+            const newItem = document.createElement('li');
+            newItem.classList.add('list__item__style');
+            newItem.classList.add(`list__item--${i + 1}`);
+            newItem.innerHTML = `<p class="card__book__name">${el.Name}
+
         </p ><p class="card__book__author">- ${el.author}
         </p><p class="card__page__number">${el.pages} pages</p>`;
 
-        // const divButtons
-        const divButtons = document.createElement('div');
-        divButtons.classList.add('card__buttons');
-        // button read status
-        const newRead = document.createElement('button');
-        newRead.classList.add('btn');
-        newRead.classList.add('card__boolean');
-        newRead.textContent = el.read === true ? 'Read ✔️' : 'Unread ❌';
-        // button delete
-        const btnDelete = document.createElement('button');
-        btnDelete.classList.add('btn');
-        btnDelete.classList.add('button__delete');
-        btnDelete.textContent = 'Delete Book';
-        // append buttons to div
-        divButtons.appendChild(newRead);
-        divButtons.appendChild(btnDelete);
-        // append div to li
-        newItem.appendChild(divButtons);
-        // append li to ul
-        list.appendChild(newItem);
-        // delete item
-        btnDelete.addEventListener('click', deleteBookCard(i));
-        // change read status
-        newRead.addEventListener('click', toggleReadStatus(el, newRead));
-    });
-};
+            // const divButtons
+            const divButtons = document.createElement('div');
+            divButtons.classList.add('card__buttons');
 
-// submit form
-formado.addEventListener('submit', submitForm());
+            // button read status
+            const newRead = document.createElement('button');
+            newRead.classList.add('btn');
+            newRead.classList.add('card__boolean');
+            newRead.textContent = el.read === true ? 'Read ✔️' : 'Unread ❌';
 
-// display form and modal
-btnNewBook.addEventListener('click', displayFormOverlay);
+            // button delete
+            const btnDelete = document.createElement('button');
+            btnDelete.classList.add('btn');
+            btnDelete.classList.add('button__delete');
+            btnDelete.textContent = 'Delete Book';
 
-// remove form and modal
-overlay.addEventListener('click', removeformOverlay);
-closeIcon.addEventListener('click', removeformOverlay);
+            // append buttons to div
+            divButtons.appendChild(newRead);
+            divButtons.appendChild(btnDelete);
 
-window.addEventListener('keydown', removeFormOverlayWindow());
+            // append div to li
+            newItem.appendChild(divButtons);
+
+            // append li to ul
+            list.appendChild(newItem);
+
+            // delete item
+            btnDelete.addEventListener('click', this.deleteBookCard(i).bind(this));
+
+            // change read status
+            newRead.addEventListener(
+                'click',
+                this.toggleReadStatus(el, newRead).bind(this)
+            );
+        });
+    };
+}
+
+const bookLibrary = new Library();
